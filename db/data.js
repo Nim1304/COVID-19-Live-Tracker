6965733bb68737d1962567ff3eac1b3c87d740da
Nim1304 <nimishshah2000@gmail.com>
2020-04-03 00:09:20 +0530

    Added Yesterday

--- db/data.js
+++ db/data.js
@@ -1,4 +1,4 @@
-const request= require('request');
+const request = require('request');
 const cheerio = require('cheerio');
 
 const URL = 'https://www.worldometers.info/coronavirus/'
@@ -7,24 +7,28 @@ const keys = ["country", "totalCases", "newCases", "totalDeaths", "newDeaths", "
     "activeCases", "serious", "casesPerMn", "deathsPerMn", "reportFirst"]
 
 
-const getData = new Promise((resolve,reject) => {
-    var endObject = []
-    var intermediateObject = {}
-    request(URL, (err, result, html) => {
-        if (!err) { 
-            const $ = cheerio.load(html);
-            $("#main_table_countries_today").find('tr').each((i, trElement) => {
-                intermediateObject = {}
-                $(trElement).find('td').each((i, tdElement) => {
-                    keys[i] == 'reportFirst' ? intermediateObject[keys[i]] = $(tdElement).text().replace('\n', '') :
-                        intermediateObject[keys[i]] = $(tdElement).text();
-                })
-                endObject.push(intermediateObject);
-            });
-            endObject.length>0 ? resolve(endObject) : reject(new Error('Server Not responding.Try after some time'));
-        }
+const getData = (yesterday) => {
+    var decider;
+    yestreday ? decider = "#main_table_countries_yesterday" : decider = "#main_table_countries_today"
+    return new Promise((resolve, reject) => {
+        var endObject = []
+        var intermediateObject = {}
+        request(URL, (err, result, html) => {
+            if (!err) {
+                const $ = cheerio.load(html);
+                $(decider).find('tr').each((i, trElement) => {
+                    intermediateObject = {}
+                    $(trElement).find('td').each((i, tdElement) => {
+                        keys[i] == 'reportFirst' ? intermediateObject[keys[i]] = $(tdElement).text().replace('\n', '') :
+                            intermediateObject[keys[i]] = $(tdElement).text();
+                    })
+                    endObject.push(intermediateObject);
+                });
+                endObject.length > 0 ? resolve(endObject) : reject(new Error('Server Not responding.Try after some time'));
+            }
+        });
     });
-});
+}
 
 
-module.exports = {getData:getData}
+module.exports = { getData: getData }
