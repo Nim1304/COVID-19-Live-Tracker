e0c3f19f1572f01897dd79f89584ccad7b6e1769
Nim1304 <nimishshah2000@gmail.com>
2020-04-08 23:30:39 +0530

    menubar changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 import Data from './component/dataCOVID.component';
-
+import MenuBar from './component/menuBar.component';
 
 function App() {
   return (
     <div>
+      <div id="menu">
+        <MenuBar />
+      </div>
       <Data />
     </div>
   );
--- client/src/component/menuBar.component.js
+++ client/src/component/menuBar.component.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 
-export default class MenuBar extends Component{
-    constructor(props){
+export default class MenuBar extends Component {
+    constructor(props) {
         super(props);
 
     }
 
-    
-
-
-
-
-    render(){
+    render() {
         return (
-            <div>
-
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <div className="input-field">
+                        <input id="search" type="search" required />
+                        <label className="label-icon" for="search"><i className="material-icons">search</i></label>
+                        <i className="material-icons">close</i>
+                    </div>
+                </div>
+            </nav>
         )
     }
 }
