8c0fa5cc4c317e17a7906e769e48b2c77e9a859b
Nimish Shah <nimishshah2000@gmail.com>
2020-04-15 21:45:42 +0530

    route changes

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const route = express.Router();
-const getData = require('../db/data').getData;
-
+// const getData = require('../db/data').getData;
 
 route.get('/',(req,res)=>{
-    getData(false).then((data)=>{
-        res.send(data);
-    }).catch((err)=>{
-        res.send(err);
-    });
+    const data = require('../data.json').data;
+    res.send(data);
 });
 
 route.get('/yesterday',(req,res)=>{
-    getData(true).then((data)=>{
-        res.send(data);
-    }).catch((err)=>{
-        res.send(err);
-    });
+    // getData(true).then((data)=>{
+    //     res.send(data);
+    // }).catch((err)=>{
+    //     res.send(err);
+    // });
+    const dataYes = require('../dataYesterday.json').data;
+    res.send(dataYes);
 })
 
 module.exports = route;
