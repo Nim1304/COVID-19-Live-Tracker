5d066d0e9220c4b8766a39b9391eb9d420ff1c08
Nimish Shah <nimishshah2000@gmail.com>
2020-04-15 23:34:40 +0530

    Revert "heroku bug"
    This reverts commit 5475ac1425ec68e1bb83989248bf2a0282a3b10e.

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended:true
 }));
-app.use(express.static(__dirname));
 app.use(express.static('client/build'));
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'client','build','index.html'))
